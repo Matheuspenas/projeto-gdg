27ed262c5daa539fa862446f27c254558a1ede9d
Paulovcarraro <paulovcarraro@gmail.com>
2025-04-01 10:52:33 -0300

    Criação da seção de contagem para o dia do evento, porém com ajustes de responsividade a serem feitos

--- JavaScript/inscrever.js
+++ JavaScript/inscrever.js
@@ -26,3 +26,35 @@ function toggleMenu() {
 function closeMenu() {
   document.getElementById("sidebar").classList.remove("active");
 }
+
+// Defina a data do evento
+const eventDate = new Date("2025-05-24T09:00:00").getTime();
+
+// Atualiza a contagem regressiva a cada segundo
+const countdown = setInterval(function () {
+  // Pega data e horas atuais
+  const now = new Date().getTime();
+
+  // Calcula a diferença entre a data do evento e a data atual
+  const distance = eventDate - now;
+
+  // Calcula os dias, horas, minutos e segundos restantes
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Exibe os resultados
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  // Se a contagem regressiva terminar, exibe uma mensagem
+  if (distance < 0) {
+    clearInterval(countdown);
+    document.getElementById("timer").innerHTML = "O Evento Começou !";
+  }
+}, 1000);
