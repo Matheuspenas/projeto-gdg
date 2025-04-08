f574a1ba9d046bf60432ae46c0def5a6cd2048da
Paulovcarraro <paulovcarraro@gmail.com>
2025-04-08 14:21:30 -0300

    Adicção de animação 3d no background da página inicial, para teste e pequena mudança na resposnividade da programação.css

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -5,3 +5,17 @@ function toggleMenu() {
 function closeMenu() {
   document.getElementById("sidebar").classList.remove("active");
 }
+
+// Script de animação do background da página 
+
+VANTA.NET({
+  el: "#vanta-net",
+  mouseControls: true,
+  touchControls: true,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0x0d6efd, // azul
+  backgroundColor: 0x000000
+})
